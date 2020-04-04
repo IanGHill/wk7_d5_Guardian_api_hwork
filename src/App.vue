9ca21e589d9c5c8bf20b59b37d8a76bd149af0cb
IanGHill <ian.g.hill@outlook.com>
2020-04-04 17:34:06 +0100

    two fetches working

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Guardian News Trawler</h1>
     <div>
       <article-list :articleList='articles'></article-list>
-      <article-detail></article-detail>
+      <article-detail :article='articleDetail'></article-detail>
     </div>  
 
   </div>
@@ -13,6 +13,7 @@
 import ArticleSearch from './components/ArticleSearch.vue';
 import ArticleList from './components/ArticleList.vue';
 import ArticleDetail from './components/ArticleDetail.vue';
+import { eventBus } from './main.js';
 
 export default {
   name: 'app',
@@ -20,21 +21,31 @@ export default {
     return {
       response: {},
       articles: [],
-      selectedArticle: null
+      selectedArticle: null,
+      articleDetail: {}
     }
   },
   mounted(){
-    fetch('https://content.guardianapis.com/search?q=brexit&page=1&page-size=25&show-tags=contributor&format=json&api-key=test')
+    fetch('https://content.guardianapis.com/search?q=brexit&page=1&page-size=25&show-tags=contributor&format=json&api-key=6421a937-fac8-4f40-887c-eeddc9bcda34')
     .then(results => results.json())
     // .then(response => this.response = response.response)
     // .then(articles => this.articles = this.response.results)
     .then(articles => this.articles = articles.response.results)
     .catch(err => console.log(err))
 
-    // https://content.guardianapis.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target?show-fields=all&api-key=test
-
+    eventBus.$on('article-selected', (article) => {
+      this.selectedArticle = article
+    })
     
-  } ,
+  },
+  watch: {
+    selectedArticle: function (oldValue, newValue){
+          fetch(`${this.selectedArticle.apiUrl}?show-fields=all&api-key=6421a937-fac8-4f40-887c-eeddc9bcda34`)
+          .then(results => results.json())
+          .then(articles => this.articleDetail = articles.response)
+          .catch(err => console.log(err))
+    }
+  },
   components: {
     "article-search": ArticleSearch,
     "article-list": ArticleList,
--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,10 +1,13 @@
 <template>
-  
+  <div v-if='article'>
+
+  </div>
 </template>
 
 <script>
 export default {
-
+    name: 'article-detail',
+    props: ['article']
 }
 </script>
 
